cfceaf17d437f2f14ba1bb082763c35d205b6221
GastonSKL <gastonlskamlec@gmail.com>
2022-11-16 12:56:04 -0300

    Commit 2
    Creation of functions to create, update and delete data, and a DB

--- src/Components/CrudApi.jsx
+++ src/Components/CrudApi.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const CrudApi = () => {
+  const [db, setDb] = useState([]);
+  const [dataEdit, setDataEdit] = useState(null);
+
+  const createData = (info) => {
+    info.id = Date.now();
+    setDb([...db, info]);
+  };
+
+  const updateData = (info) => {
+    let newData = db.map((el) => (el.id === info.id ? info : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm('Are you sure you want to delete?');
+    if (!isDelete) {
+      return;
+    }else{
+        let newData = db.filter((el)=> el.id !== id);
+        setDb(newData);
+    }
+  }
+
   return (
     <div>
-        <h2>CRUD API</h2>
+      <h2>CRUD API </h2>
+      <article>
+        <form></form>
+        <table></table>
+      </article>
     </div>
-  )
-}
+  );
+};
